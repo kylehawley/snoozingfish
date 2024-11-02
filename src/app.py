2bd30975e124eebc6262e5d94657507186de6740
kylehawley <152016473+kylehawley@users.noreply.github.com>
2024-11-02 12:36:37 +0100

    moving over to flask

---
+++ src/app.py
@@ -0,0 +1,41 @@
+import sqlite3
+
+from flask import Flask, jsonify, render_template, request
+
+app = Flask(__name__)
+
+
+def get_db_connection():
+    conn = sqlite3.connect("listings.db")
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+@app.route("/")
+def index():
+    conn = get_db_connection()
+    listings = conn.execute("SELECT * FROM listings WHERE is_new = 1").fetchall()
+    conn.close()
+    return render_template("index.html", listings=listings)
+
+
+@app.route("/nl-listings")
+def listings_page():
+    conn = get_db_connection()
+    listings = conn.execute("SELECT * FROM listings WHERE is_new = 1").fetchall()
+    conn.close()
+    return render_template("listings.html", listings=listings)
+
+
+@app.route("/apply/<int:id>", methods=["POST"])
+def apply_to_listing(id):
+    print("trying to apply to listing")
+    conn = get_db_connection()
+    conn.execute("UPDATE listings SET is_new = 0 WHERE id = ?", (id,))
+    conn.commit()
+    conn.close()
+    return jsonify({"success": True})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
